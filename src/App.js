0463c49e583415b79c13e32f39fa44c999597d24
Saimen-Luo <lswofficial@163.com>
2019-06-06 10:54:32 +0800

    antd import as needed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 // App组件
 import React, { Component } from 'react'
+import { Button, message } from 'antd'
+
 export default class App extends Component {
+    handleClick = () => {
+        message.info('普通消息')
+    }
     render() {
         return (
-            <div>App</div>
+            <div>App<Button type="primary" onClick={this.handleClick}>按钮</Button></div>
         )
     }
 }
